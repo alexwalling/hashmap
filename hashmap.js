d4c55b7ec23f5cb97ef9848b87de31a9375c4275
alexwalling <alex.walling19@gmail.com>
2015-10-13 13:32:39 -0600

    class implementation

--- hashmap.js
+++ hashmap.js
@@ -1,109 +1,106 @@
-var hashmap;
+function map(){
+	this.arr = [];
 
-//hash function from darkskyapp
-//find original hash function at https://github.com/darkskyapp/string-hash
-function hash(str) {
-  var h = 5381,
-      i = str.length;
+	//hash function from darkskyapp
+	//find original hash function at https://github.com/darkskyapp/string-hash
+	this.hash = function (str) {
+		var h = 5381, i = str.length;
 
-  while(i){
-    h = (h * 33) ^ str.charCodeAt(--i);
-  }
-  /* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
-   * integers. Since we want the results to be always positive, convert the
-   * signed int to an unsigned by doing an unsigned bitshift. */
-  return h >>> 0;
-}
-
-function constructor(size){
-	var tempHashMap = new Array(size);
-	return tempHashMap;
-}
-//link string key to arbitrary value
-function set(key, value){
-	if(key == null){
-		return false;
-	}
-
-	var newEntry = {};
-	var index = hash(key) % hashmap.length;
-	newEntry[key] = value;
-
-	if(hashmap[index] == undefined || hashmap[index] == 'closed'){
-		hashmap[index] = newEntry;
-		return true;
-	}
-	while(hashmap[index] != undefined){
-		if(index == hashmap.length - 1){
-			index = 0;
-		} else {
-			index++;
+		while(i){
+			h = (h * 33) ^ str.charCodeAt(--i);
 		}
-		if(hashmap[index] == undefined){
-			hashmap[index] = newEntry;
+		/* JavaScript does bitwise operations (like XOR, above) on 32-bit signed
+		* integers. Since we want the results to be always positive, convert the
+		* signed int to an unsigned by doing an unsigned bitshift. */
+		return h >>> 0;
+	};
+
+	this.constructor = function (size){
+		this.arr = new Array(size);
+	};
+	//link string key to arbitrary value
+	this.set = function (key, value){
+		if(key == null){
+			return false;
+		}
+
+		var newEntry = {};
+		var index = this.hash(key) % this.arr.length;
+		newEntry[key] = value;
+
+		if(this.arr[index] == undefined || this.arr[index] == 'closed'){
+			this.arr[index] = newEntry;
 			return true;
 		}
-	}
-	return false;
-}
-
-function get(key){
-	var index = hash(key) % hashmap.length;
-
-}
-
-function del(key){
-	var index = hash(key) % hashmap.length;
-	if(hashmap[index] == undefined){
-		return null;
-	}
-	while(hashmap[index] != undefined){
-		if(typeof hashmap[index] == 'object' && Object.keys(hashmap[index]) == key){
-			var r = hashmap[index][key];
-			hashmap[index] = 'closed';
-			return r;
+		while(this.arr[index] != undefined){
+			if(index == this.arr.length - 1){
+				index = 0;
+			} else {
+				index++;
+			}
+			if(this.arr[index] == undefined){
+				this.arr[index] = newEntry;
+				return true;
+			}
 		}
-		if(index == hashmap.length - 1){
-			index = 0;
-		} else {
-			index++;
+		return false;
+	};
+
+	this.get = function (key){
+		var index = this.hash(key) % this.arr.length;
+	};
+
+	this.del = function (key){
+		var index = this.hash(key) % this.arr.length;
+		if(this.arr[index] == undefined){
+			return null;
 		}
-	}
-}
+		while(this.arr[index] != undefined){
+			if(typeof this.arr[index] == 'object' && Object.keys(this.arr[index]) == key){
+				var r = this.arr[index][key];
+				this.arr[index] = 'closed';
+				return r;
+			}
+			if(index == this.arr.length - 1){
+				index = 0;
+			} else {
+				index++;
+			}
+		}
+	};
 
-function load(){
-	return float;
-}
+	this.load = function (){
+		return float;
+	};
 
-function print(){
-	console.log('printing array:');
-	for(i = 0; i < hashmap.length; i++){
-		console.log('i: ', i);
-		console.log('val: ', hashmap[i]);
-	}
-	console.log();
+	this.print = function (){
+		console.log('printing array:');
+		for(i = 0; i < this.arr.length; i++){
+			console.log('i: ', i);
+			console.log('val: ', this.arr[i]);
+		}
+		console.log();
+	};
 }
-
-hashmap = constructor(10);
+hashmap = new map();
+hashmap.constructor(10);
 console.log('length: ',hashmap.length);
-
-console.log('test: ', hash('a') % hashmap.length);
 
 console.log();
 
-set('a',10); //hash of a == 4
-set('a',11);
-set('a',12);
+hashmap.set('a',10); //hash of a == 4
+hashmap.set('a',11);
+hashmap.set('a',12);
 
 console.log(hashmap[4]);
 
 console.log();
 
-print();
-del('a');
-print();
-del('a');
-print();
+hashmap.print();
+hashmap.del('a');
+hashmap.print();
+hashmap.del('a');
+hashmap.print();
 
-set('a',34);
-print();
+hashmap.set('a',34);
+hashmap.print();
